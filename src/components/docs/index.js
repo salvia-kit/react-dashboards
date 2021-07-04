e509c5f61480f6c2148caf5a30597400122fce4c
enochndika <enochbeloved@gmail.com>
2021-07-04 19:05:13 +0100

    add component section in documentation

--- src/components/docs/index.js
+++ src/components/docs/index.js
@@ -309,6 +309,24 @@ export default function Docs() {
           </li>
         </ul>
       </section>
+
+      <section className="px-3 md:px-8 mt-16">
+        <h2 className="text-2xl font-medium">Components</h2>
+        <p className="mt-5">
+          If you'll need some UI components for Tailwind CSS such as modal,
+          accordion, dropdown, tab or other components, you can check out
+          <a
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://kimia-ui.vercel.app"
+            className="text-blue-500 px-1 font-medium"
+          >
+            Kimia-UI
+          </a>
+          , a collection of fully customizable UI components, without any
+          installation, just copy and paste the component you want to use.
+        </p>
+      </section>
     </div>
   );
 }
