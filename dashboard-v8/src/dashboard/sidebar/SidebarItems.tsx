7f5c64eb758f754448f1ba31dcb0ee0404e315e4
Enoch Ndika <enochbeloved@gmail.com>
2023-07-13 22:25:43 +0100

    add dashboard v8

---
+++ dashboard-v8/src/dashboard/sidebar/SidebarItems.tsx
@@ -0,0 +1,42 @@
+import { Link, useLocation } from "react-router-dom";
+import { data } from "./data";
+import { Collapse } from "./Collapse";
+
+const style = {
+  active: "font-normal mx-4 text-sm text-blue-600",
+  inactive: "font-light mx-4 text-sm text-gray-900",
+  link: "inline-flex items-center justify-start my-1 p-3 text-black",
+};
+
+export function SidebarItems() {
+  const { pathname } = useLocation();
+  return (
+    <ul className="mt-6 md:pl-6">
+      <li>
+        {data.map(({ section, icon, content }) => (
+          <Collapse key={section}>
+            <div className="flex">
+              <span>{icon}</span>
+              <span className="pl-3">{section}</span>
+            </div>
+            {content.map((item) => (
+              <div className="pl-5" key={item.title}>
+                <Link to={item.link}>
+                  <span className={style.link}>
+                    <span
+                      className={
+                        item.link === pathname ? style.active : style.inactive
+                      }
+                    >
+                      {item.title}
+                    </span>
+                  </span>
+                </Link>
+              </div>
+            ))}
+          </Collapse>
+        ))}
+      </li>
+    </ul>
+  );
+}
