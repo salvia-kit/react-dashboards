24f90e51b1c7437e20ca580ec85b0489453aa9bc
Enoch Ndika <enochbeloved@gmail.com>
2023-07-13 22:24:38 +0100

    add dashboard v4

---
+++ dashboard-v4/src/dashboard/sidebar/SidebarItems.tsx
@@ -0,0 +1,42 @@
+import { Link, useLocation } from "react-router-dom";
+import { data } from "./data";
+import { useDashboardContext } from "../Provider";
+
+const style = {
+  title: "mx-4 text-sm whitespace-pre",
+  active: "bg-gray-700 rounded-full",
+  link: "flex items-center justify-start my-1 p-3 w-full hover:text-white",
+  close:
+    "lg:duration-700 lg:ease-out lg:invisible lg:opacity-0 lg:transition-all",
+  open: "lg:duration-500 lg:ease-in lg:h-auto lg:opacity-100 lg:transition-all lg:w-auto",
+};
+
+export function SidebarItems() {
+  const { pathname } = useLocation();
+  const { sidebarOpen } = useDashboardContext();
+
+  return (
+    <ul className="md:pl-3">
+      {data.map((item) => (
+        <li key={item.title}>
+          <Link to={item.link}>
+            <span className={style.link}>
+              <div
+                className={`p-2 ${item.link === pathname ? style.active : ""}`}
+              >
+                <span>{item.icon}</span>
+              </div>
+              <span
+                className={`${style.title} ${
+                  sidebarOpen ? style.open : style.close
+                }`}
+              >
+                {item.title}
+              </span>
+            </span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
