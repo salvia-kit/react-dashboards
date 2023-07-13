4c2442e2d60a586412edf9147fafd372c34f7069
Enoch Ndika <enochbeloved@gmail.com>
2023-07-13 22:25:14 +0100

    add dashboard v6

---
+++ dashboard-v6/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: [
+    "react-refresh",
+    "@typescript-eslint",
+    "unused-imports",
+    "tailwindcss",
+  ],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
---
+++ dashboard-v6/src/dashboard/Provider.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+interface DashboardProviderProps {
+  children: React.ReactNode;
+}
+
+interface ProviderValues {
+  sidebarOpen?: boolean;
+  openSidebar?: () => void;
+  closeSidebar?: () => void;
+}
+
+// create new context
+const Context = React.createContext<ProviderValues>({});
+
+export function DashboardProvider({ children }: DashboardProviderProps) {
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
+  const location = useLocation();
+
+  const openSidebar = React.useCallback(() => {
+    setSidebarOpen(true);
+  }, []);
+
+  const closeSidebar = React.useCallback(() => {
+    setSidebarOpen(false);
+  }, []);
+
+  // set the html tag overflow to hidden
+  React.useEffect(() => {
+    document.documentElement.style.overflow = "hidden";
+  }, []);
+
+  // close Sidebar on route changes when viewport is less than 1024px
+  React.useEffect(() => {
+    return () => {
+      if (sidebarOpen && window.innerWidth < 1024) {
+        setSidebarOpen(false);
+      }
+    };
+  }, [location, sidebarOpen]);
+
+  return (
+    <Context.Provider value={{ sidebarOpen, openSidebar, closeSidebar }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+// custom hook to consume all context values { open, ref, toggle }
+export function useDashboardContext() {
+  return React.useContext(Context);
+}
